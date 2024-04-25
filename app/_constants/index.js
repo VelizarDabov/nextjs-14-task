1ae1166f4544a8664ce49aec6b862010711bc7c9
Velizar Dabov <velizardabov@gmail.com>
2024-04-25 15:17:36 +0300

    ok

---
+++ app/_constants/index.js
@@ -0,0 +1,31 @@
+
+export const cardsData = [
+  {
+    title: "Brief",
+    image: "/research.png",
+    description: (
+      <span>
+        Complete <b>brief writing or simple guidance</b> on what to include, we've got you covered.
+      </span>
+    ),
+  },
+  {
+    title: "Search",
+    image: "/presentation.png",
+    description: (
+      <span>
+        In-depth agency search covering; <b>criteria matching</b>, door knocking and due-diligence vetting.
+      </span>
+    ),
+  },
+  {
+    title: "Pitch",
+    image: "/brainstorming.png",
+    description: (
+      <span>
+        Comprehensive <b>pitch management,</b> including comms, diary management and pitch hosting.
+      </span>
+    ),
+  },
+];
+
